6832eb44720d7e954143e077fd1a8acbf5a9b5c3
shadan <shadankarim08@gmail.com>
2022-09-09 18:39:40 +0530

    Testing.......

--- Path/script.js
+++ Path/script.js
@@ -1180,7 +1180,7 @@ $(window).on('load',function(){
 
 $(window).on('load',function(){
 		$('#exampleModalLong').modal('disp');
-		$('#exampleModalLong').modal('displayed');
-        $('#exampleModalLong').modal('displaying');
+		$('#exampleModalLong').modal('displayed in ph');
+        $('#exampleModalLong').modal('displaying ph');
 });
 
